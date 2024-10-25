0c4cbfe20d337462f4d957b7cf045e2c38bbc6a7
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-25 09:25:50 +0200

    Basic backend structure and user schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 const app = express();
 
+app.use("/api/auth", authRoutes)
+
 app.listen(3000, () => {
+  connectDB()
   console.log(`Server is listening on port 3000`);
 });
