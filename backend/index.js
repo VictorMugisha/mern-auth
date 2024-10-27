b62b69a78a27afd9fb2a9daf846038859f75e030
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-27 19:07:22 +0200

    Implment auth.controller signup function

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,15 @@ import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json());
 
-app.listen(3000, () => {
-  connectDB()
-  console.log(`Server is listening on port 3000`);
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is listening on port ${PORT}`);
 });
