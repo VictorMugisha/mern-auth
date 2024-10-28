4edd177f268a304c928582ad55943b48e2758afd
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-28 18:19:45 +0200

    Login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -94,9 +94,44 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login route");
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalide Credentials" });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalide Credentials" });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = Date.now();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Login successfuly",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login controller: ", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("logout route");
+  res.clearCookie("auth_token_jwt");
+  res.status(200).json({ success: true, message: "Logout successfuly" });
 };
