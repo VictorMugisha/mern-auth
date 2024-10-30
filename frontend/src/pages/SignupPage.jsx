97fc9321640d2f099bd0f95e354c62fa40dda523
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-10-30 16:59:12 +0200

    Build a basic structure of signup page UI

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,13 @@
 import { motion } from "framer-motion";
+import { Lock, Mail, User } from "lucide-react";
+import Input from "../components/Input";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function SignupPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   function handleSignup(event) {
     event.preventDefault();
   }
@@ -17,7 +24,47 @@ export default function SignupPage() {
           Create Account
         </h2>
 
-        <form onSubmit={handleSignup}></form>
+        <form onSubmit={handleSignup}>
+          <Input
+            icon={User}
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            icon={Mail}
+            type="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            icon={Lock}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {/* Password strength meter */}
+          <motion.button
+            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+          >
+            Signup
+          </motion.button>
+        </form>
+      </div>
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Already have an account?{"  "}
+          <Link to="/login" className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
       </div>
     </motion.div>
   );
